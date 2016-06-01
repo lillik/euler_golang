5050d28366be4637d1993a045debd484db5c029e
lillik <lilian.codreanu@gmail.com>
2016-06-01 18:33:45 +0300

    Add files via upload

---
+++ problem5/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main()  {
+	fmt.Println("Problem 4");
+	t0 := time.Now()
+	//
+	var number int = 0;
+	var found bool = true;
+	for i:=2520; ; i += 20 {
+		found = true;
+		for j:=2; j<=20; j++ {
+			if i % j !=0 {
+				found = false;
+			}
+		}
+		if found == true {
+			number = i;
+			break;
+		}
+
+	}
+	fmt.Println("The number is:",number);
+	t1 := time.Now()
+	fmt.Printf("The call took %v to run.\n", t1.Sub(t0))
+}
